7a61a047510274db537d68ff3df8b62f92489061
Mijgona <mijgon.9191@gmail.com>
2021-02-05 11:27:18 +0500

    coding animation

--- src/App.js
+++ src/App.js
@@ -16,19 +16,23 @@ function App() {
   return (
     <> 
     <center>
-      <div>
-      <center>      
+      <div className='col-6 shadow-lg box m-5' >
+        <center>
+        <div className='col-8 m-5 box'>        
         <div>
-          <Canvas  style={{ width:'100%', height:'100%' }}camera={{ position: [-1, 0.5, 2.5], fov: 90 }}>            
+          <Canvas  style={{ width:'100%', height:'100%' }}camera={{ position: [-1, 0.5, 2.5], fov: 90 }}
+              >            
               <pointLight position={[-20, -20, 50]} />            
               <spotLight position={[-2, 10, 0]} />
               <Box  position={[0, 0, 1]} fliping={flip} />
-          </Canvas>         
-          <button  onClick={handleStart}>Бросить монету</button>
+        </Canvas> 
+        
+        <button className="bton m-5 shadow" onClick={handleStart}>Бросить монету</button>
         </div>
-      </center>
-      </div>
-    </center>
+          </div>
+          </center>
+          </div>
+          </center>
     </>
   );
 }
