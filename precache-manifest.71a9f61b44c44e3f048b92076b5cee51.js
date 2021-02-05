eea30303ba7a6845c3ebb8609699eec6a217e020
Mijgona <mijgon.9191@gmail.com>
2021-02-06 04:22:12 +0500

    Updates

---
+++ precache-manifest.71a9f61b44c44e3f048b92076b5cee51.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ccf2af165654eba23714874c5bf0f6",
+    "url": "/coinflip/index.html"
+  },
+  {
+    "revision": "cf9c201d881bdf256da9",
+    "url": "/coinflip/static/css/2.83531460.chunk.css"
+  },
+  {
+    "revision": "77b1634cfb081e77e509",
+    "url": "/coinflip/static/css/main.1efcebe0.chunk.css"
+  },
+  {
+    "revision": "cf9c201d881bdf256da9",
+    "url": "/coinflip/static/js/2.2d05ed9c.chunk.js"
+  },
+  {
+    "revision": "ecd188f4da4367c6309279f499a5356f",
+    "url": "/coinflip/static/js/2.2d05ed9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b1634cfb081e77e509",
+    "url": "/coinflip/static/js/main.86744bff.chunk.js"
+  },
+  {
+    "revision": "cc7b0095f7fbb009f693",
+    "url": "/coinflip/static/js/runtime-main.398d7465.js"
+  },
+  {
+    "revision": "bddb65fa1d6285c50062b1bcb2fc994f",
+    "url": "/coinflip/static/media/nside.bddb65fa.png"
+  },
+  {
+    "revision": "bf1c688e053d1f8e5e859698e48f0f80",
+    "url": "/coinflip/static/media/yside.bf1c688e.png"
+  }
+]);
